8e1ae366faf55608c5256de1890762f819d2e9eb
Jason Hulbet <hulbert.jason@gmail.com>
2022-06-01 12:56:24 -0400

    - Adds template-based theme files and python script theme generator - WIP on light theme colors - WIP on accessible theme colors

---
+++ theme.py
@@ -0,0 +1,29 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+import yaml
+
+themes = {
+    'default': 'default.yaml',
+    'light': 'light.yaml'
+}
+
+for name in themes:
+    with open(f'src/data/{themes[name]}') as theme_data_file:
+        theme_data = yaml.safe_load(theme_data_file)
+
+    env = Environment(
+        loader=PackageLoader('theme', 'src/templates'),
+        autoescape=select_autoescape()
+    )
+
+    env.filters['bool'] = bool
+
+    theme_template = env.get_template('theme.json.template')
+    theme_scheme_template = env.get_template('scheme.xml.template')
+
+    with open(f'src/main/resources/{theme_data["theme_out_file"]}', 'w+') as theme_out_file:
+        theme_out_file.write(theme_template.render(theme_data))
+        theme_out_file.close()
+
+    with open(f'src/main/resources/{theme_data["scheme_out_file"]}', 'w+') as scheme_out_file:
+        scheme_out_file.write(theme_scheme_template.render(theme_data))
+        scheme_out_file.close()
